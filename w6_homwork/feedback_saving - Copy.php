51ddc74202dbd0417dec45fe4004d6e255311645
Aurimas <lokis065@gmail.com>
2018-08-13 14:09:02 +0300

    improvment

--- w6_homwork/feedback_saving - Copy.php
+++ w6_homwork/feedback_saving - Copy.php
@@ -10,9 +10,9 @@ $error = array("name" => "","email" => "", "feedback" => "","database" => "");
 if($_POST){
     if(strlen($name)>0 && strlen($feedback)>0 && strpos($email,"@") && is_numeric($number1) && is_numeric($number2) && ($number1)+($number2)== 10) {
 
-        // Attempt MySQL server connection to the db feedback
+        // Attempt MySQL server connection to db feedback
         $conn = new mysqli('localhost','root','root','feedback'); 
-            //checking conection 
+            //checking conection
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
                 }
@@ -20,20 +20,12 @@ if($_POST){
         $saved = $conn->query("INSERT INTO `feedback`(`name`, `email`, `feedback`) VALUES ('$name','$email','$feedback')");
             if($saved){
                     header('Location:' . $_SERVER['PHP_SELF'] . '?success=OK'); 
-            }else{ 
-				$error['database'] = "Error when saving"; //if not save to DB
+            }else{
+				$error['database'] = "Error when saving";
 			}
 
-    } else { //cheking input one by one
-        if(strlen($name) == 0){
-            $error['name'] = 'Error';
-        }
-        if(strlen($email) == 0){
-            $error['email'] = 'Error';
-        }
-        if(strlen($feedback) == 0){
-            $error['feedback'] = 'Error';
-        }
+    } else {
+    print "There was error";    
     }
 }
 if(strlen($success) == 0) {
@@ -44,51 +36,36 @@ if(strlen($success) == 0) {
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>html->php->form->db</title>
+    <title>html->php->form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--<link rel="stylesheet" type="text/css" media="screen" href="style.css" />-->
     <style>
                 body {
             margin: 50px;
         }
-        input[type=text], input[type=email], textarea {
+        input, textarea {
             display: block;
             min-width: 30%;
             margin: 20px 0;
             padding: 5px;
         }
-        input[type=submit] {
-            padding:5px 15px; 
-            background:#ccc; 
-            border:0 none;
-            cursor:pointer;
-            border-radius: 5px; 
-        }
-        span {
-            color: red;
-            display: inline;
-            font-weight: bold;
-        }
     </style>
 </head>
 <body>
-    <h1>Feedback form save to DB</h1>
+    <h1>Feedback form</h1>
     <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-        Name <span><?php echo $error['name']; ?></span><input type="text" name="name" placeholder="Add Name" 
-            value="<?php echo $name;?>" />
-        Email <span><?php echo $error['email']; ?></span><input type="email" name="email" placeholder="Add Email"
-            value="<?php echo $email;?>"/>
-        Feedback <span><?php echo $error['feedback']; ?></span><textarea name="feedback" placeholder="Add Your feedback"
-            value="<?php echo $feedback;?>"></textarea>
+        Name<input type="text" name="name" placeholder="Add Name"/>
+        Email<input type="text" name="email" placeholder="Add Email"/>
+        Feedback<textarea name="feedback" placeholder="Add Your feedback"></textarea>
         Number1<input type="text" name="number1" placeholder="Number1 + Number2 must by 10" />
         Number2<input type="text" name="number2" placeholder="Number2 + Number1 must by 10"/>
-        <span><?php echo $error['database']; ?></span>
+        <?php echo $error['database']; ?>
         <input type="submit" value="Save"/>
     </form>
 </body>
 </html>
 <?php 
   } else {
-	  print "OK feedback saved to DB";
+	  print "OK";
   }
 ?>	  
